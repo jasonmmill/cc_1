c851bf1265735a01ffaddc3ca01e7ca40624f8b3
Jasonmmill <jasonmatthewmill@gmail.com>
2025-01-25 22:18:37 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables
+let employeeName = "Markum Reed"
+const employeeID = 1
+var isActive = true
+console.log(employeeName)
+console.log(typeof(employeeName))
+console.log(employeeID)
+console.log(typeof(employeeID))
+console.log(isActive)
+console.log(typeof(isActive))
+
